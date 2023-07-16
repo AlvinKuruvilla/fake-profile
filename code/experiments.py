1cce6626267ccb6c7b6b365291ec11207ca3c8ac
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2023-07-16 09:57:41 -0400

    Fix the simple cross platform test

--- code/experiments.py
+++ code/experiments.py
@@ -11,7 +11,7 @@ def print_k_table(matrix, ids):
     rows.append([3, top_k_accuracy_score(np.array(ids), np.array(matrix), k=3)])
     rows.append([4, top_k_accuracy_score(np.array(ids), np.array(matrix), k=4)])
     rows.append([5, top_k_accuracy_score(np.array(ids), np.array(matrix), k=5)])
-    table = tabulate(rows, headers=["K", "Score"], tablefmt="orgtbl")
+    table = tabulate(rows, headers=["K", "Score"], tablefmt="plain")
     print(table)
 
 
@@ -51,17 +51,31 @@ def train_session_one_test_two():
 def simple_cross_platform():
     heatmap = HeatMap(VerifierType.Similarity)
 
-    matrix = heatmap.combined_keystroke_matrix(1, 1, None, None, 1)
-    matrix2 = heatmap.combined_keystroke_matrix(2, 2, None, None, 1)
-    matrix3 = heatmap.combined_keystroke_matrix(3, 3, None, None, 1)
+    matrix = heatmap.combined_keystroke_matrix(1, 2, None, None, 1)
+    matrix2 = heatmap.combined_keystroke_matrix(1, 3, None, None, 1)
+    matrix3 = heatmap.combined_keystroke_matrix(2, 1, None, None, 1)
+    matrix4 = heatmap.combined_keystroke_matrix(2, 3, None, None, 1)
+    matrix5 = heatmap.combined_keystroke_matrix(3, 1, None, None, 1)
+    matrix6 = heatmap.combined_keystroke_matrix(3, 2, None, None, 1)
     ids = [num for num in range(1, 26) if num != 22]
     print()
-    print("Facebook")
+    print("FI")
     print_k_table(matrix=matrix, ids=ids)
-    print("Instagram")
+    input()
+    print("FT")
     print_k_table(matrix=matrix2, ids=ids)
-    print("Twitter")
+    input()
+    print("IF")
     print_k_table(matrix=matrix3, ids=ids)
+    input()
+    print("IT")
+    print_k_table(matrix=matrix4, ids=ids)
+    input()
+    print("TF")
+    print_k_table(matrix=matrix5, ids=ids)
+    input()
+    print("TI")
+    print_k_table(matrix=matrix6, ids=ids)
 
 
 def cross_platform_2v1():
@@ -79,4 +93,4 @@ def cross_platform_2v1():
     print_k_table(matrix=matrix3, ids=ids)
 
 
-same_platform_even_split()
+simple_cross_platform()
