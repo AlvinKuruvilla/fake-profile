e3f04d74dc699571fd7b46b8e82274926659d4a4
rk042 <rajeshjnu2006@gmail.com>
2023-07-30 10:19:04 -0400

    added Pickle

--- utility/Pickle.py
+++ utility/Pickle.py
@@ -1,14 +1,26 @@
 import pickle
+
+
 class PickleHelper:
     def __init__(self):
-        pass
-    def save_to(location, obj):
+        self.message = "this is a test message"
+
+    def save_to(self, location, obj):
         f = open(location, "wb")
         pickle.dump(obj, f)
         f.close()
 
-    def load_back(location):
+    def load_back(self, location):
         f = open(location, 'rb')
         g = pickle.load(f)
         f.close()
         return g
+
+
+if __name__ == '__main__':
+    # local testing
+    PickleObj = PickleHelper()
+    print(PickleObj.message)
+    PickleObj.save_to('PickleObj.b', PickleObj)
+    PickleObj2 = PickleObj.load_back('PickleObj.b')
+    print(PickleObj2.message)
