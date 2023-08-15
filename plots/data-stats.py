d379f8d4cbfc4f066622e4483a6289a4d032f24f
rk042 <rajeshjnu2006@gmail.com>
2023-08-15 12:48:03 -0400

    data stats
    ecdf

--- plots/data-stats.py
+++ plots/data-stats.py
@@ -1,26 +1,71 @@
-# Plot adjustments
-fig, axs = plt.subplots(1, 4, figsize=(20, 5))
+import matplotlib.pyplot as plt
+import numpy as np
 
-# Gender pie chart
-axs[0].pie(sizes_gender, labels=labels_gender, autopct='%1.1f%%', startangle=90, colors=['#ff9999','#66b2ff','#99ff99'])
-axs[0].set_title('Gender Distribution')
+# Data
+labels_gender = ['M', 'F', 'O']
+sizes_gender = [38, 54, 8]
 
-# Handedness pie chart
-axs[1].pie(sizes_handedness, labels=labels_handedness, autopct='%1.1f%%', startangle=90, colors=['#ffcc99', '#c2c2f0'])
-axs[1].set_title('Handedness Distribution')
+labels_handedness = ['Left', 'Right']
+sizes_handedness = [13.6, 86.4]
 
-# Platform bar chart
-axs[2].bar(labels_platform, sizes_platform, color=['#ff9999','#66b2ff','#99ff99'])
-axs[2].set_title('Average Keystrokes per Platform')
-axs[2].set_ylabel('Average Keystrokes')
-axs[2].set_xlabel('Platform')
+labels_platform = ['Facebook', 'Instagram', 'Twitter']
+sizes_platform = [1154, 859, 706]
 
-# Session bar chart
-axs[3].bar(labels_session, sizes_session, color='#c2c2f0')
-axs[3].set_title('Average Keystrokes per Session')
-axs[3].set_ylabel('Average Keystrokes')
-axs[3].set_xlabel('Session Number')
+labels_session = ['1', '2', '3', '4', '5', '6']
+sizes_session = [2462, 1981, 1983, 1787, 1809, 1730]
 
-# Adjust layout
+# Adjusting the plot layout to keep one pie and one bar chart next to each other
+fig, axs = plt.subplots(1, 4, figsize=(24, 6))
+
+# Adjusting the color palette for better visibility and aesthetics
+colors_adjusted = {
+    'medium_blue': '#3a6ea5',
+    'lighter_blue': '#6b9ed6',
+    'peach': '#ed8a63',
+    'tan': '#e9bf77',
+    'gray': '#a8a8a8'
+}
+
+# Avg keystrokes per platform Bar Chart
+axs[0].bar(labels_platform,
+           sizes_platform,
+           color=[colors_adjusted['lighter_blue'], colors_adjusted['medium_blue'], colors_adjusted['tan']])
+axs[0].set_title('Avg keystrokes per platform')
+axs[0].set_ylabel('Avg keystrokes')
+axs[0].set_xlabel('platform')
+
+
+# Gender Distribution Pie Chart with labels on the graph
+axs[1].pie(sizes_gender,
+           autopct='%1.1f%%',
+           startangle=90,
+           colors=[colors_adjusted['medium_blue'], colors_adjusted['tan'], colors_adjusted['gray']],
+           wedgeprops=dict(width=0.1))
+axs[1].set_title('Gender')
+axs[1].legend(labels_gender, loc="best")
+
+
+# Handedness Distribution Pie Chart with labels on the graph
+axs[2].pie(sizes_handedness,
+           autopct='%1.1f%%',
+           startangle=90,
+           colors=[colors_adjusted['medium_blue'], colors_adjusted['peach']],
+           wedgeprops=dict(width=0.1))
+axs[2].set_title('Handedness')
+axs[2].legend(labels_handedness, loc="best")
+
+
+
+# Avg keystrokes per session Bar Chart with adjusted width
+bar_width = .75  # Making the bar thinner
+axs[3].bar(labels_session,
+           sizes_session,
+           color=colors_adjusted['medium_blue'],
+           width=bar_width)
+axs[3].set_title('Avg keystrokes per video')
+axs[3].set_ylabel('Avg keystrokes')
+axs[3].set_xlabel('video #')
+
+# Adjusting the layout for better presentation
 plt.tight_layout()
 plt.show()
---
+++ plots/ecdf_per_user_keystrokes.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import seaborn as sns
+# Data
+user_ids = [i for i in range(1, 27) if i != 22]  # Excluding 22 as it's missing from the data
+sample_counts = [1193, 755, 814, 745, 426, 867, 376, 1138, 587, 1132,
+                 1122, 1128, 1871, 1194, 931, 742, 984, 703, 2537,
+                 768, 1043, 930, 699, 512, 362]
+
+# Calculate ECDF
+x = np.sort(sample_counts)
+y = np.arange(1, len(x) + 1) / len(x)
+
+# Defining the color palette
+colors_adjusted = {
+    'medium_blue': '#3a6ea5',
+    'lighter_blue': '#6b9ed6'
+}
+
+from scipy.interpolate import make_interp_spline
+# Interpolating the data for a smoother curve
+x_new = np.linspace(min(x), max(x), 500)  # Create 500 evenly spaced values within the range of x
+y_new = np.interp(x_new, x, y)  # Interpolate a smooth curve based on original x and y
+
+# Using a spline interpolation to further smooth the ECDF
+x_spline = np.linspace(x_new.min(), x_new.max(), 1000)
+spline = make_interp_spline(x_new, y_new, k=3)  # Using a 3rd degree spline
+y_spline = spline(x_spline)
+
+# Plotting the further smoothed ECDF
+plt.figure(figsize=(12, 7))
+sns.set_style("whitegrid")
+plt.plot(x_spline, y_spline, linestyle='-', color=colors_adjusted['lighter_blue'])
+plt.fill_between(x_spline, y_spline, color=colors_adjusted['lighter_blue'], alpha=0.2)
+plt.title('ECDF', fontsize=16)
+plt.xlabel('Avg. keystrokes', fontsize=14)
+plt.ylabel('F(x)', fontsize=14)
+plt.xticks(fontsize=12)
+plt.yticks(fontsize=12)
+plt.grid(True, which="both", ls="--", c='0.7')
+
+# Polishing the plot
+sns.despine(left=True, bottom=True)
+
+plt.show()
